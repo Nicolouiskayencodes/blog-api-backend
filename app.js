4a3c846a624ae144a5901396734f00683bbf7962
Nicolas Louis-Kayen <nicolouiskayencodes@gmail.com>
2024-11-19 23:46:00 -0800

    Delete commented lines

--- app.js
+++ app.js
@@ -8,12 +8,7 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
-// app.use(bodyParser.json());
-// app.use(function(req, res, next) {
-//   res.header("Access-Control-Allow-Origin", "*");
-//   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-//   next();
-// });
+
 app.use(routes)
 
 app.use((err, req, res, next) => {
